40fc80b4edc3efafe6f47e6d3bc677e4fd243d20
Gen Reynolds <genmreynolds@hotmail.co.uk>
2019-03-19 16:53:54 +0000

    Initial disclosure code

--- src/Disclosure.py
+++ src/Disclosure.py
@@ -1,6 +1,50 @@
 import Algorithmia
+import pandas as pd
+import numpy as np
+from io import StringIO
+import json
 
-# API calls will begin at the apply() method, with the request body passed as 'input'
-# For more details, see algorithmia.com/developers/algorithm-development/languages
 def apply(input):
-    return "hello {}".format(input)
+    cur_period = 201706
+    bob = disclosure_stage_1(cur_period)
+    return bob
+
+def disclosure_stage_1(cur_period):
+    #client = Algorithmia.client()
+    #file = client.file("data://gmreynolds/TestData/vfs.csv")
+
+
+    file = "C:\\Users\\Off.Network.User9\\Desktop\\disclosure\\src\\vfs.csv"
+    df = pd.read_csv(file, dtype={"Q601_asphalting_sand": int, 'Q602_building_soft_sand': int,
+                                             'Q603_concreting_sand': int, 'Q604_bituminous_gravel': int,
+                                             'Q605_concreting_gravel': int, 'Q606_other_gravel': int,
+                                             'Q607_constructional_fill': int, 'Q608_total': int,
+                                             'enterprise_ref': int, 'period': int, 'region': int})
+
+    df["disclosive"] = None
+    df["publish"] = None
+    df["reason"] = None
+
+    def runDisclosure(row):
+
+        if(row['Q608_total'] == 0):
+            row['disclosive'] = 'N'
+            row['publish'] = 'Publish'
+            row['reason'] = 'Stage 1 - Q608 total is zero'
+        else:
+            row['disclosive'] = 'Y'
+            row['publish'] = 'N/A'
+        return row
+
+    disaggregatedData = df[df.period == cur_period]
+
+    regionagg = disaggregatedData.groupby('region')
+
+
+    regionagg = regionagg.agg({'Q608_total': 'sum', 'Q607_constructional_fill': 'sum', 'Q606_other_gravel': 'sum', 'Q605_concreting_gravel': 'sum', 'Q604_bituminous_gravel': 'sum', 'Q603_concreting_sand': 'sum', 'Q602_building_soft_sand': 'sum', 'Q601_asphalting_sand': 'sum', 'enterprise_ref': 'nunique'})
+    regionagg = regionagg.apply(runDisclosure, axis=1)
+    print("end result\n", regionagg)
+    regionlorm = disaggregatedData.groupby(['region'])
+    regionagglorm = disaggregatedData.groupby(['region', 'land_or_marine'])
+    print("this is regionlorm\n", regionagg)
+    print("this is regionagglorm\n", regionagglorm)
